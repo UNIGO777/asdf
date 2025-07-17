347c11878e2fb6f9973f05d12cc7d49065c25bf4
UNIGO777 <naman13399@gmail.com>
2025-07-18 00:51:20 +0530

    aksdljf

--- src/Home page/Hero.jsx
+++ src/Home page/Hero.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
-import HeroVideo from '../assets/HeroVideo.mp4'
+
 
 const Hero = () => {
   const [heroData, setHeroData] = useState({
